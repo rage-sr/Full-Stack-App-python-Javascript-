386561293d9cf04b1b1299e349880e546b87e1fa
rage-sr <ragecrazy2829@gmail.com>
2025-03-26 14:33:12 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   }, []);
 
   const fetchContacts = async () => {
-    const response = await fetch("http://192.168.1.15:5000/contacts");
+    const response = await fetch("http://127.0.0.1:5000/contacts");
     const data = await response.json();
     setContacts(data.contacts);
   };
